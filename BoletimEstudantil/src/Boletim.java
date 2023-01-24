604944d7d686688b5bc59dcd9af7991676cd7e4d
felipevinicios <felipe.1577@outlook.com>
2023-01-24 15:28:35 -0300

    Atividade boletim

---
+++ BoletimEstudantil/src/Boletim.java
@@ -0,0 +1,18 @@
+
+public class Boletim {
+
+	public static void main(String[] args) {
+		int mediaFinal = 6;
+		
+		if(mediaFinal <6)
+		System.out.println("Reprovado");
+		
+		else if (mediaFinal==6)
+			System.out.println("Prova Minerva");
+		else
+			System.out.println("Aprovado");
+		
+		
+		
+	}
+}
